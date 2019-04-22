69ddb578329280376c4275874415092f3bbc8f6d
Christian Siewert <christian_siewert@gmx.de>
2019-04-22 09:55:35 +0200

    cs

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -19,8 +19,8 @@ if (!array_key_exists('APP_ENV', $_SERVER)) {
 
 if ('prod' !== $_SERVER['APP_ENV']) {
     if (!class_exists(Dotenv::class)) {
-        $message = 'The "APP_ENV" environment variable is not set to "prod". ' .
-            'Please run "composer require symfony/dotenv" to load the ".env" files ' .
+        $message = 'The "APP_ENV" environment variable is not set to "prod". '.
+            'Please run "composer require symfony/dotenv" to load the ".env" files '.
             'configuring the application.';
         throw new RuntimeException($message);
     }
@@ -30,6 +30,5 @@ if ('prod' !== $_SERVER['APP_ENV']) {
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?: $_ENV['APP_ENV'] ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-
-$filterVar = filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN);
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || $filterVar ? '1' : '0';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] ||
+    filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
