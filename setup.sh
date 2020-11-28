fde66e8965848258da51568b37c055bc5d5c73d6
christiansiewert <christian.siewert@protonmail.com>
2020-11-28 10:35:12 +0000

    fixed error in rsa generation

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@
 composer install
 
 # Generate OpenSSL certificate
-openssl genrsa -out config/jwt/private.pem -aes256 4096 -passout pass:app!
-openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem -aes256 4096 -passin pass:app!
+openssl genrsa -out config/jwt/private.pem -aes256 -passout pass:app! 4096
+openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem -passin pass:app!
 
 # Generate OpenSSL certificate for our test suite
-docker-compose exec php openssl genrsa -out config/jwt/private-test.pem -aes256 4096  -passout pass:app!
-docker-compose exec php openssl rsa -pubout -in config/jwt/private-test.pem -out config/jwt/public-test.pem  -passin pass:app!
+openssl genrsa -out config/jwt/private-test.pem -aes256 -passout pass:app! 4096
+openssl rsa -pubout -in config/jwt/private-test.pem -out config/jwt/public-test.pem -passin pass:app!
 
 # Create databases
 mysql -e 'CREATE DATABASE app;'
